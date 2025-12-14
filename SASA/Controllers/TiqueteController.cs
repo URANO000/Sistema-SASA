38aafbb6cd566931f4dbaa1de9e0f69e3305d6b1
URANO000 <kkonigin1100@gmail.com>
2025-12-14 03:29:45 -0600

    Table...not very pretty yet

---
+++ SASA/Controllers/TiqueteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SASA.Controllers
+{
+    public class TiqueteController : Controller
+    {
+        //GET: TiqueteController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
