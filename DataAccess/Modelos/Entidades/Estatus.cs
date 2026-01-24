0f3a2b86d4235dabae4d4b4d9b0523f834106d7a
URANO000 <kkonigin1100@gmail.com>
2026-01-24 02:09:44 -0600

    Estatus entity

--- DataAccess/Modelos/Entidades/Estatus.cs
+++ DataAccess/Modelos/Entidades/Estatus.cs
@@ -1,8 +1,19 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAccess.Modelos.Entidades
 {
+    [Table("Estatus")]
     public class Estatus
     {
+        [Key]
+        [Column("idEstatus")]
+        public int IdEstatus { get; set; }
+
+        [Column("nombreEstatus")]
+        public string NombreEstatus { get; set; }
+
+        //Collection -> Relacion con tiquetes
+        public ICollection<Tiquete>? Tiquete { get; set; }
     }
 }
