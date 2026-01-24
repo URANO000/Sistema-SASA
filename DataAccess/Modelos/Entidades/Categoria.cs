1f008f068c6872825fd78510fbe18eb589facc6b
URANO000 <kkonigin1100@gmail.com>
2026-01-24 01:54:20 -0600

    Categoria entity

--- DataAccess/Modelos/Entidades/Categoria.cs
+++ DataAccess/Modelos/Entidades/Categoria.cs
@@ -1,7 +1,19 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DataAccess.Modelos.Entidades
 {
+    [Table("CATEGORIA")]
     public class Categoria
     {
+        [Key]
+        [Column("idCategoria")]
+        public int IdCategoria { get; set; }
+
+        [Column("nombreCategoria")]
+        public string NombreCategoria { get; set; }
+
+        //Collection -> Relacion con tiquetes
+        public ICollection<Tiquete>? Tiquete { get; set; }
     }
 }
