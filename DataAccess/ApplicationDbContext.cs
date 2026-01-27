b9ddfaa62983297852afafb4dfae8804cfa35694
URANO000 <kkonigin1100@gmail.com>
2026-01-27 02:03:11 -0600

    Repository Tiquete
    Implemented a bit of stuff for cancel action. This, utilizing an enum to define the ids. If change is ever needed, it is to be done to the enum directly.

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Modelos.Entidades;
+using DataAccess.Modelos.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess
@@ -16,6 +17,16 @@ namespace DataAccess
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         { //esto es para configurar las entidades y relaciones (por si lo ocupan o sino hagan caso omiso)
             base.OnModelCreating(modelBuilder);
+
+            //Seeder de enum
+            modelBuilder.Entity<Estatus>()
+                .HasData(
+                    new Estatus { IdEstatus = (int)TiqueteEstatus.Abierto, NombreEstatus = "Abierto" },
+                    new Estatus { IdEstatus = (int)TiqueteEstatus.EnProgreso, NombreEstatus = "En Progreso"},
+                    new Estatus { IdEstatus = (int)TiqueteEstatus.Cancelado, NombreEstatus = "Cancelado"},
+                    new Estatus { IdEstatus = (int)TiqueteEstatus.Resuelto, NombreEstatus = "Resuelto"},
+                    new Estatus { IdEstatus = (int)TiqueteEstatus.Cerrado, NombreEstatus = "Cerrado"}
+                );
         } 
     }
 
--- DataAccess/Repositorios/Tiquetes/TiqueteRepository.cs
+++ DataAccess/Repositorios/Tiquetes/TiqueteRepository.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataAccess.Modelos.Entidades;
+using DataAccess.Modelos.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Repositorios.Tiquetes
 {
-    public class TiqueteRepository
+    public class TiqueteRepository : ITiqueteRepository
     {
+        //Referencia al contexto de la base de datos
+        private readonly ApplicationDbContext _context;
+
+        public TiqueteRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Implementación de los métodos del repositorio de tiquetes
+        public async Task<IEnumerable<Tiquete>> ObtenerTiquetesAsync()
+            => await _context.Tiquetes.ToListAsync();
+
+        public async Task<Tiquete?> ObtenerTiquetePorIdAsync(int id)
+            => await _context.Tiquetes.FindAsync(id);
+
+        public async Task AgregarTiqueteAsync(Tiquete tiquete)
+        {
+            _context.Tiquetes.Add(tiquete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ActualizarTiqueteAsync(Tiquete tiquete)
+        {
+            _context.Tiquetes.Update(tiquete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task CancelarTiquete(int id)
+        {
+            var tiquete = await _context.Tiquetes.FindAsync(id);
+            if(tiquete != null)
+            {
+                tiquete.IdEstatus = (int)TiqueteEstatus.Cancelado;
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
