5429f8def115d51389472f412b297e03a5a854f9
URANO000 <kkonigin1100@gmail.com>
2026-01-29 01:28:15 -0600

    Creation of Dtos

---
+++ DataAccess/Modelos/DTOs/Tiquete/CrearTiqueteAdminDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccess.Modelos.DTOs.Tiquete
+{
+    public class CrearTiqueteAdminDto
+    {
+        public required string Asunto { get; init; }
+        public required string Descripcion { get; init; }
+        public required int IdCategoria { get; init; }
+        public int? IdPrioridad { get; init; }
+        public int? IdCola { get; init; }
+        public int? IdAsignee { get; init; }
+        public int? IdReportedBy { get; init; }
+    }
+}
