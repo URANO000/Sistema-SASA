179c267e6d9988dcd3cc758477c6f6c5f911c9fc
URANO000 <kkonigin1100@gmail.com>
2026-01-30 11:10:15 -0600

    Update CrearUsuarioViewModel.cs

--- SASA/ViewModels/Usuario/CrearUsuarioViewModel.cs
+++ SASA/ViewModels/Usuario/CrearUsuarioViewModel.cs
@@ -5,12 +5,16 @@ namespace SASA.ViewModels.Usuario
 {
     public sealed class CrearUsuarioViewModel
     {
-        public CrearUsuarioDto Usuario { get; init; } = new CrearUsuarioDto
-        {
-            PrimerNombre = string.Empty,
-            PrimerApellido = string.Empty,
-            CorreoEmpresa = string.Empty
-        };
-        public IReadOnlyList<SelectListItem>? Roles { get; init; }
+        public required string PrimerNombre { get; set; }
+        public string? SegundoNombre { get; set; }
+        public required string PrimerApellido { get; set; } 
+        public string? SegundoApellido { get; set; }
+        public required string CorreoEmpresa { get; set; } 
+
+        public required string Departamento { get; set; }
+        public required string Puesto { get; set; }
+
+        public List<string> RolesSeleccionados { get; set; } = [];
+        public IReadOnlyList<SelectListItem>? RolesDisponibles { get; set; }
     }
 }
