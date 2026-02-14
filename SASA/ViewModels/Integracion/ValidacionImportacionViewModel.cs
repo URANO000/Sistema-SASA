2b4ccd5d4170440c75e2a8f548f7626c3a300970
Pablo Arias <parias40359@ufide.ac.cr>
2026-02-14 10:30:19 -0600

    I will do a pull/merge from dev so i need to commit this sh

---
+++ SASA/ViewModels/Integracion/ValidacionImportacionViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace SASA.ViewModels.Integracion
+{
+    public class ValidacionImportacionViewModel
+    {
+        public int HistorialId { get; set; }
+        public string NombreArchivo { get; set; } = string.Empty;
+
+        public int TotalFilas { get; set; }
+        public int FilasValidas { get; set; }
+        public int FilasConError { get; set; }
+
+        public List<FilaValidacion> Errores { get; set; } = new();
+        public List<FilaImportacionActivos> FilasValidasPreview { get; set; } = new();
+    }
+
+    public class FilaValidacion
+    {
+        public int NumeroFila { get; set; }
+        public string Mensaje { get; set; } = string.Empty;
+    }
+
+    public class FilaImportacionActivos
+    {
+        public int NumeroFila { get; set; }
+        public string NumeroActivo { get; set; } = string.Empty;
+        public string NombreMaquina { get; set; } = string.Empty;
+        public string TipoEquipo { get; set; } = string.Empty;
+
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+        public string? SerieServicio { get; set; }
+        public string? DireccionMAC { get; set; }
+        public string? SistemaOperativo { get; set; }
+
+        public string? TipoLicencia { get; set; }
+        public string? ClaveLicencia { get; set; }
+    }
+}
