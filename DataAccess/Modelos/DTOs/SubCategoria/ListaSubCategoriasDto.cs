c2fedeb80e41f7cdf9af2d2b7c44ad50c9e535ab
URANO000 <kkonigin1100@gmail.com>
2026-03-12 02:39:38 -0600

    Delete ListaSubCategoriasDto.cs

--- DataAccess/Modelos/DTOs/SubCategoria/ListaSubCategoriasDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DataAccess.Modelos.DTOs.SubCategoria
-{
-    public class ListaSubCategoriasDto
-    {
-        public int IdSubCategoria { get; set; }
-        public string NombreSubCategoria { get; set; } = string.Empty;
-    }
-}
