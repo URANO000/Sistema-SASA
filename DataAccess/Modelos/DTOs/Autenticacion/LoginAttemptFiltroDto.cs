e2225e65585adb2e1ad68c91b4a58d7862171bef
mhlebens <marifermatah@gmail.com>
2026-03-14 16:58:11 -0600

    working on the loginAttempt, 65% already done

---
+++ DataAccess/Modelos/DTOs/Autenticacion/LoginAttemptFiltroDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccess.Modelos.DTOs.Autenticacion
+{
+    public class LoginAttemptFiltroDto
+    {
+        public string? EmailIngresado { get; set; }
+        public bool? Exitoso { get; set; }
+        public DateTime? FechaDesde { get; set; }
+        public DateTime? FechaHasta { get; set; }
+
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 15;
+    }
+}
